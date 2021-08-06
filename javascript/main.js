2389883a561c46d3d3c866ec6f631bee303dba7a
Vishal wakpaijan <VW56500@gmail.com>
2021-08-06 13:09:56 +0530

    added mobile searchInput

--- javascript/main.js
+++ javascript/main.js
@@ -30,3 +30,21 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+const searchBtn = document.getElementById("searchBtn");
+const searchBar = document.getElementById("searchBar");
+const overlay = document.getElementById("overlay");
+
+searchBtn.addEventListener("click", () => {
+  searchBar.classList.add("searchBar_active");
+  overlay.classList.add("overlay_active");
+});
+
+overlay.addEventListener("click", () => {
+  searchBar.classList.remove("searchBar_active");
+  overlay.classList.remove("overlay_active");
+});
+
+const submitFunction = () => {
+  alert("Thank you for visting");
+};
